2f0da010fd33ec42cb1ee1b2dc1b2eb9b529f185
kontrec91 <kontrec91@gmail.com>
2024-10-09 16:40:20 +0300

    feat: added Collapse table

--- src/components/PlaylistTableTest.tsx
+++ src/components/PlaylistTableTest.tsx
@@ -5,6 +5,7 @@ import {
  getExpandedRowModel,
  flexRender,
  ColumnDef,
+ Row,
 } from "@tanstack/react-table";
 import {
  AllPlaylistsDataType,
@@ -12,13 +13,124 @@ import {
  PlaylistTableTestProps,
  TrackDataType,
 } from "../redux/types";
-// import { ElderlyRounded, GMobiledata } from "@mui/icons-material";
+import {
+ Box,
+ Collapse,
+ IconButton,
+ Table,
+ TableBody,
+ TableCell,
+ TableHead,
+ TableRow,
+} from "@mui/material";
+import { alpha } from "@mui/material/styles";
+import { KeyboardArrowUp, KeyboardArrowDown } from "@mui/icons-material";
 
-export const PlaylistTableTest = ({ data }: PlaylistTableTestProps) => {
+const ExpandedPlaylistRow = (
+ row: Row<AllPlaylistsDataType | PlaylistDataType | TrackDataType>
+) => {
  const [expandedPlaylistId, setExpandedPlaylistId] = useState<string | null>(
   null
  );
 
+ const playlists = "subRows" in row.original ? row.original.subRows : [];
+
+ function func(playlist: PlaylistDataType) {
+  if (playlist.subRows.length === 0) {
+   return null;
+  }
+  return expandedPlaylistId === playlist.playlistId ? (
+   <KeyboardArrowUp />
+  ) : (
+   <KeyboardArrowDown />
+  );
+ }
+
+ if (!playlists.length) {
+  return null;
+ }
+
+ return playlists.map((playlist) => {
+  if ("playlistName" in playlist) {
+   const playlistId = playlist.playlistId;
+   return (
+    <>
+     <TableRow key={playlistId}>
+      <TableCell sx={{ padding: "0" }}>
+       <IconButton
+        onClick={() =>
+         setExpandedPlaylistId((prev) =>
+          prev === playlistId ? null : playlistId
+         )
+        }>
+        {func(playlist)}
+       </IconButton>
+      </TableCell>
+      <TableCell
+       style={{
+        cursor: "pointer",
+        padding: "5px 0",
+       }}>
+       {playlist.playlistName}
+      </TableCell>
+     </TableRow>
+     <TableRow>
+      <TableCell sx={{ paddingTop: "0", paddingBottom: "0" }}>
+       <Collapse in={expandedPlaylistId === playlistId} unmountOnExit>
+        <Table>
+         <TableBody>
+          {playlist.subRows.map((track) => (
+           <TableRow key={track.trackId}>
+            <TableCell>{track.originalFileName}</TableCell>
+           </TableRow>
+          ))}
+         </TableBody>
+        </Table>
+       </Collapse>
+      </TableCell>
+     </TableRow>
+    </>
+   );
+  }
+  return null;
+ });
+
+ //    return <TableRow>
+ //    <TableCell
+ //     colSpan={row.getVisibleCells().length}
+ //     sx={{ padding: "0px", paddingLeft: "20px" }}>
+ //     <Table sx={{ width: "100%" }}>
+ //      <TableBody>
+ //       {"subRows" in row.original &&
+ //        (row.original.subRows as PlaylistDataType[]).map(
+ //         (playlist: PlaylistDataType) => {
+ //          console.log("row", row);
+ //          console.log("cells", row.getVisibleCells());
+ //          return (
+ //           expandedPlaylistId === playlist.playlistId &&
+ //           playlist.subRows.map((track: TrackDataType) => (
+ //            <TableRow key={track.trackId}>
+ //             <TableCell
+ //              sx={{
+ //               padding: "5px",
+ //               border: "1px solid black",
+ //               cursor: "pointer",
+ //              }}>
+ //              {track.originalFileName}
+ //             </TableCell>
+ //            </TableRow>
+ //           ))
+ //          );
+ //         }
+ //        )}
+ //      </TableBody>
+ //     </Table>
+ //    </TableCell>
+ //   </TableRow>
+ //  )
+};
+
+export const PlaylistTableTest = ({ data }: PlaylistTableTestProps) => {
  const columns = useMemo<
   ColumnDef<AllPlaylistsDataType | PlaylistDataType | TrackDataType>[]
  >(
@@ -31,45 +143,10 @@ export const PlaylistTableTest = ({ data }: PlaylistTableTestProps) => {
    {
     header: "Playlist Name",
     accessorKey: "playlistName",
-    cell: ({ row }) => {
-     const playlists = "subRows" in row.original ? row.original.subRows : [];
-     console.log("playlists", playlists);
-     // Отображаем плейлисты
-     if (!playlists.length) {
-      return null;
-     }
-     return playlists.map((playlist) => {
-      if ("playlistName" in playlist) {
-       console.log('"playlistName" in playlist', playlist);
-       return (
-        <div
-         key={playlist.playlistId}
-         onClick={() => {
-          const playlistId = playlist.playlistId;
-          if (playlist.subRows.length) {
-           setExpandedPlaylistId((prev) =>
-            prev === playlistId ? null : playlistId
-           );
-          }
-         }}
-         style={{
-          cursor: "pointer",
-          padding: "5px 0",
-         }}>
-         {!!playlist.subRows.length
-          ? `${expandedPlaylistId === playlist.playlistId ? "▼" : "▶"}`
-          : null}
-         {playlist.playlistName}
-        </div>
-       );
-      }
-      console.log("tracks is here, render null");
-      return null;
-     });
-    },
+    cell: ({ row }) => ExpandedPlaylistRow(row),
    },
   ],
-  [expandedPlaylistId]
+  []
  );
 
  const table = useReactTable({
@@ -88,70 +165,36 @@ export const PlaylistTableTest = ({ data }: PlaylistTableTestProps) => {
  });
 
  return (
-  <>
-   <table style={{ width: "100%", border: "1px solid black" }}>
-    <thead>
-     {table.getHeaderGroups().map((headerGroup) => (
-      <tr key={headerGroup.id}>
-       {headerGroup.headers.map((header) => (
-        <th
-         key={header.id}
-         style={{ padding: "10px", borderBottom: "2px solid black" }}>
-         {flexRender(header.column.columnDef.header, header.getContext())}
-        </th>
-       ))}
-      </tr>
-     ))}
-    </thead>
-    <tbody>
-     {table.getRowModel().rows.map((row) => (
-      <React.Fragment key={row.id}>
-       <tr>
-        {/* playlists */}
-        {row.getVisibleCells().map((cell) => {
-         console.log("cell.getContext()", cell.getContext().row.original);
-         console.log("cell.column.columnDef.cell", cell.column.columnDef.cell);
-         return (
-          <td
-           key={cell.id}
-           style={{ padding: "10px", border: "1px solid black" }}>
-           {flexRender(cell.column.columnDef.cell, cell.getContext())}
-          </td>
-         );
-        })}
-       </tr>
-       {/* tracks */}
-       {expandedPlaylistId && (
-        <tr>
-         <td colSpan={row.getVisibleCells().length}>
-          <table style={{ width: "100%", paddingLeft: "20px" }}>
-           <tbody>
-            {"subRows" in row.original &&
-             (row.original.subRows as PlaylistDataType[]).map(
-              (playlist: PlaylistDataType) => {
-               console.log("row", row);
-               console.log("cells", row.getVisibleCells());
-               return (
-                expandedPlaylistId === playlist.playlistId &&
-                playlist.subRows.map((track: TrackDataType) => (
-                 <tr key={track.trackId}>
-                  <td style={{ padding: "5px", border: "1px solid black" }}>
-                   {track.originalFileName}
-                  </td>
-                 </tr>
-                ))
-               );
-              }
-             )}
-           </tbody>
-          </table>
-         </td>
-        </tr>
-       )}
-      </React.Fragment>
-     ))}
-    </tbody>
-   </table>
-  </>
+  <Table
+   sx={{
+    width: "100%",
+    border: "1px solid black",
+    bgcolor: (theme) => alpha(theme.palette.background.paper, 0.7),
+   }}>
+   <TableHead>
+    {table.getHeaderGroups().map((headerGroup) => (
+     <TableRow key={headerGroup.id}>
+      {headerGroup.headers.map((header) => (
+       <TableCell
+        key={header.id}
+        sx={{ padding: "10px", borderBottom: "2px solid black" }}>
+        {flexRender(header.column.columnDef.header, header.getContext())}
+       </TableCell>
+      ))}
+     </TableRow>
+    ))}
+   </TableHead>
+   <TableBody>
+    {table.getRowModel().rows.map((row) => (
+     <TableRow>
+      {row.getVisibleCells().map((cell) => (
+       <TableCell key={cell.id} sx={{ border: "1px solid black" }}>
+        {flexRender(cell.column.columnDef.cell, cell.getContext())}
+       </TableCell>
+      ))}
+     </TableRow>
+    ))}
+   </TableBody>
+  </Table>
  );
 };
